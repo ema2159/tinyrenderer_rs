c6dbf5577d759c19c0738262c7f3016d62623f7a
Emmanuel Bustos <ema2159@gmail.com>
2022-11-23 00:37:34 +0200

    Add: Implement basic window drawing system

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
+extern crate image;
 extern crate piston_window;
+
+use image::{Rgba, RgbaImage};
+use piston_window::EventLoop;
 
 const WIDTH: u32 = 1280;
 const HEIGHT: u32 = 720;
 
 fn main() {
+    let img = RgbaImage::from_pixel(WIDTH, HEIGHT, Rgba([255, 0, 0, 255]));
+
     let mut window: piston_window::PistonWindow =
         piston_window::WindowSettings::new("tinyrenderer_rs", [WIDTH, HEIGHT])
             .exit_on_esc(true)
             .build()
             .unwrap_or_else(|_e| panic!("Could not create window!"));
-    while let Some(_event) = window.next() {
+
+    window.set_lazy(true);
+
+    let texture = piston_window::Texture::from_image(
+        &mut window.create_texture_context(),
+        &img,
+        &piston_window::TextureSettings::new(),
+    ).unwrap();
+
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |c, g, _| {
+            piston_window::clear([0.0, 0.0, 0.0, 1.0], g);
+            piston_window::image(&texture, c.transform, g);
+        });
     }
 }
