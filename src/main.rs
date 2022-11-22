a834762615f9cafc6997d698aa8bfbe2621aa316
Emmanuel Bustos <ema2159@gmail.com>
2022-11-22 22:51:36 +0200

    Add: Basic window using piston_window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate piston_window;
+
+const WIDTH: u32 = 1280;
+const HEIGHT: u32 = 720;
+
 fn main() {
-    println!("Hello World!", );
+    let mut window: piston_window::PistonWindow =
+        piston_window::WindowSettings::new("tinyrenderer_rs", [WIDTH, HEIGHT])
+            .exit_on_esc(true)
+            .build()
+            .unwrap_or_else(|_e| panic!("Could not create window!"));
+    while let Some(_event) = window.next() {
+    }
 }
