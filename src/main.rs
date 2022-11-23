b8abd84bc5e3e2b09f539df37c1c753eb6f17db7
Emmanuel Bustos <ema2159@gmail.com>
2022-11-23 23:28:27 +0200

    Add: Implementation of Bresenham's line drawing algorithm

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,75 @@ use piston_window::EventLoop;
 const WIDTH: u32 = 1280;
 const HEIGHT: u32 = 720;
 
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+// Implementation of the Bresenham's line algorithm
+fn line(img: &mut RgbaImage, start: Point, end: Point, color: Rgba<u8>) {
+    let Point {
+        x: mut x0,
+        y: mut y0,
+    } = start;
+    let Point {
+        x: mut x1,
+        y: mut y1,
+    } = end;
+
+    let mut steep = false;
+    if (x1 - x0).abs() < (y1 - y0).abs() {
+        (x0, y0) = (y0, x0);
+        (x1, y1) = (y1, x1);
+        steep = true;
+    }
+
+    // Always draw from lowest x to highest x
+    if x1 < x0 {
+        (x1, x0) = (x0, x1);
+        (y1, y0) = (y0, y1);
+    }
+    let dx = x1 - x0;
+    let mut dy = y1 - y0;
+    let mut yi = 1;
+
+    // Allow negative slopes
+    if dy < 0 {
+        (yi, dy) = (-1, -dy)
+    };
+
+    let mut d = 2 * dy - dx;
+    let mut y = y0;
+    if steep {
+        for x in x0..=x1 {
+            img.put_pixel(y as u32, x as u32, color);
+            if d > 0 {
+                y = y + yi;
+                d = d - 2 * dx;
+            }
+            d = d + 2 * dy;
+        }
+    } else {
+        for x in x0..=x1 {
+            img.put_pixel(x as u32, y as u32, color);
+            if d > 0 {
+                y = y + yi;
+                d = d - 2 * dx;
+            }
+            d = d + 2 * dy;
+        }
+    };
+}
+
 fn main() {
     let mut img = RgbaImage::from_pixel(WIDTH, HEIGHT, Rgba([0, 0, 0, 255]));
 
-    for x in 15..=17 {
-        for y in 8..24 {
-            img.put_pixel(x, y, Rgba([255, 0, 0, 255]));
-            img.put_pixel(y, x, Rgba([255, 0, 0, 255]));
-        }
-    }
+    line(
+        &mut img,
+        Point { x: 0, y: 0 },
+        Point { x: 700, y: 719 },
+        Rgba([255, 255, 255, 255]),
+    );
 
     image::imageops::flip_vertical_in_place(&mut img);
 
