8956026bbc7c29f3e8ec1c580d77499717a75e31
Emmanuel Bustos <ema2159@gmail.com>
2022-11-24 16:48:58 +0200

    Mod: Put renderer-related code into its own module

--- src/main.rs
+++ src/main.rs
@@ -3,74 +3,80 @@ extern crate piston_window;
 
 use image::{Rgba, RgbaImage};
 use piston_window::EventLoop;
+use tinyrenderer_rs::Point;
 
 const WIDTH: u32 = 1280;
 const HEIGHT: u32 = 720;
 
-struct Point {
-    x: i32,
-    y: i32,
-}
-
-// Implementation of the Bresenham's line algorithm
-fn line(img: &mut RgbaImage, start: Point, end: Point, color: Rgba<u8>) {
-    let Point {
-        x: mut x0,
-        y: mut y0,
-    } = start;
-    let Point {
-        x: mut x1,
-        y: mut y1,
-    } = end;
-
-    let mut steep = false;
-    if (x1 - x0).abs() < (y1 - y0).abs() {
-        (x0, y0) = (y0, x0);
-        (x1, y1) = (y1, x1);
-        steep = true;
+mod tinyrenderer_rs {
+    use image::{Rgba, RgbaImage};
+    pub struct Point {
+        pub x: i32,
+        pub y: i32,
     }
 
-    // Always draw from lowest x to highest x
-    if x1 < x0 {
-        (x1, x0) = (x0, x1);
-        (y1, y0) = (y0, y1);
+    // Implementation of the Bresenham's line algorithm
+    pub fn draw_line(img: &mut RgbaImage, start: Point, end: Point, color: Rgba<u8>) {
+        let Point {
+            x: mut x0,
+            y: mut y0,
+        } = start;
+        let Point {
+            x: mut x1,
+            y: mut y1,
+        } = end;
+
+        let mut steep = false;
+        if (x1 - x0).abs() < (y1 - y0).abs() {
+            (x0, y0) = (y0, x0);
+            (x1, y1) = (y1, x1);
+            steep = true;
+        }
+
+        // Always draw from lowest x to highest x
+        if x1 < x0 {
+            (x1, x0) = (x0, x1);
+            (y1, y0) = (y0, y1);
+        }
+        let dx = x1 - x0;
+        let mut dy = y1 - y0;
+        let mut yi = 1;
+
+        // Allow negative slopes
+        if dy < 0 {
+            (yi, dy) = (-1, -dy)
+        };
+
+        let mut d = 2 * dy - dx;
+        let mut y = y0;
+        if steep {
+            for x in x0..=x1 {
+                img.put_pixel(y as u32, x as u32, color);
+                if d > 0 {
+                    y = y + yi;
+                    d = d - 2 * dx;
+                }
+                d = d + 2 * dy;
+            }
+        } else {
+            for x in x0..=x1 {
+                img.put_pixel(x as u32, y as u32, color);
+                if d > 0 {
+                    y = y + yi;
+                    d = d - 2 * dx;
+                }
+                d = d + 2 * dy;
+            }
+        };
     }
-    let dx = x1 - x0;
-    let mut dy = y1 - y0;
-    let mut yi = 1;
-
-    // Allow negative slopes
-    if dy < 0 {
-        (yi, dy) = (-1, -dy)
-    };
-
-    let mut d = 2 * dy - dx;
-    let mut y = y0;
-    if steep {
-        for x in x0..=x1 {
-            img.put_pixel(y as u32, x as u32, color);
-            if d > 0 {
-                y = y + yi;
-                d = d - 2 * dx;
-            }
-            d = d + 2 * dy;
-        }
-    } else {
-        for x in x0..=x1 {
-            img.put_pixel(x as u32, y as u32, color);
-            if d > 0 {
-                y = y + yi;
-                d = d - 2 * dx;
-            }
-            d = d + 2 * dy;
-        }
-    };
 }
+
+use tinyrenderer_rs::draw_line;
 
 fn main() {
     let mut img = RgbaImage::from_pixel(WIDTH, HEIGHT, Rgba([0, 0, 0, 255]));
 
-    line(
+    draw_line(
         &mut img,
         Point { x: 0, y: 0 },
         Point { x: 700, y: 719 },
