f0dceb0e1f59499ff42da9ef65f27bd684bc46ad
Emmanuel Bustos <ema2159@gmail.com>
2022-12-15 18:18:50 +0200

    fix: Path and add asset

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut img = RgbaImage::from_pixel(WIDTH, HEIGHT, Rgba([0, 0, 0, 255]));
 
     let obj_path =
-        Path::new("/home/ema2159/Documents/GitHub/tinyrenderer_rs/assets/african_head.obj");
+        Path::new("/home/ema2159/Documents/GitHub/tinyrenderer_rs/assets/african_head/african_head.obj");
     let input = BufReader::new(File::open(&obj_path)?);
     let model: Obj = load_obj(input)?;
 
